e203475169f52549a317749afa909d0132857039
HokageSarada <61898929+HokageSarada@users.noreply.github.com>
2024-01-07 23:48:58 +0800

    updated tests

--- e2e/pet-challenge.spec.js
+++ e2e/pet-challenge.spec.js
@@ -17,19 +17,19 @@ test("check that pet was added", async ({ request }) => {
   expect(petName).toBe(petData.name);
 });
 
-// test("updating a pet", async ({ request }) => {
-//   const response = await request.put(`pet`, {
-//     data: petUpdate,
-//   });
-//   expect(response.status()).toBe(200)
-// });
+test("updating a pet", async ({ request }) => {
+  const response = await request.put(`pet`, {
+    data: petUpdate,
+  });
+  expect(response.status()).toBe(200)
+});
 
-// test('check that pet was updated', async ({ request }) => {
-//     const response = await request.get(`pet/${petUpdate.id}`)
-//     let json = await response.json();
-//     let petName = json.name
-//     expect(petName).toBe(petUpdate.name)
-// })
+test('check that pet was updated', async ({ request }) => {
+    const response = await request.get(`pet/${petUpdate.id}`)
+    let json = await response.json();
+    let petName = json.name
+    expect(petName).toBe(petUpdate.name)
+})
 
 test("delete a pet", async ({ request }) => {
   const response = await request.delete(`pet/${petUpdate.id}`);
